d394da0c8b7d8d1416f7417e52aa1342deb03e02
ChocoMucho <49467508+ChocoMucho@users.noreply.github.com>
2024-01-18 09:41:33 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 0.29 ms, Memory: 31 MB -BaekjoonHub

---
+++ 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.cs
@@ -0,0 +1,16 @@
+using System;
+
+public class Solution {
+    public string solution(string phone_number) {
+        string answer = "";
+        
+        string str = phone_number.Substring(phone_number.Length - 4,4);
+        
+        for(int i=0; i < phone_number.Length - 4; ++i)
+        {
+            answer += "*";
+        }
+        
+        return answer+str;
+    }
+}
