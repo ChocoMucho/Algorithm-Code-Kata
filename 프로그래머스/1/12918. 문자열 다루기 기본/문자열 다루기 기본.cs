fbd5c9cb3ba255a30bc6ae64704f305a22b187d4
ChocoMucho <49467508+ChocoMucho@users.noreply.github.com>
2024-01-25 09:14:50 +0900

    Update 문자열 다루기 기본.cs

--- 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.cs
+++ 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.cs
@@ -2,11 +2,19 @@ public class Solution {
     public bool solution(string s) {
         bool answer = true;
         
-        for(int i=0; i<s.Length; ++i)
+        if(s.Length == 4 || s.Length == 6)
         {
-            if(s[i] < '0' || s[i] > '9')
-                return false;
+            for(int i=0; i<s.Length; ++i)
+            {
+                if(s[i] < '0' || s[i] > '9')
+                    return false;
+            }
         }
+        else
+        {
+            answer = false;
+        }
+        
         
         return answer;
     }
