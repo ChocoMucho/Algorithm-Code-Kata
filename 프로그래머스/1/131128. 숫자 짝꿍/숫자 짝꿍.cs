b4da9d4d1b66fb62d856a886e96bd7f33a24b8d3
ChocoMucho <49467508+ChocoMucho@users.noreply.github.com>
2024-03-20 09:31:03 +0900

    [level 1] Title: 숫자 짝꿍, Time: 0.90 ms, Memory: 31.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Linq;
+
+public class Solution {
+    public string solution(string X, string Y) {
+        string answer = "";
+        
+        int[] xArr = new int[10];
+        int[] yArr = new int[10];
+        
+        for(int i = 0; i < X.Length; ++i)
+        {
+            ++xArr[int.Parse(X[i].ToString())];
+        }
+        
+        for(int i = 0; i < Y.Length; ++i)
+        {
+            ++yArr[int.Parse(Y[i].ToString())];
+        }
+        
+        for(int i = 9; i >= 0; --i)
+        {
+            if(xArr[i] <= 0 || yArr[i] <= 0)
+                continue;
+            
+            
+            int min = xArr[i] <= yArr[i] ? xArr[i] : yArr[i];
+            
+            answer += string.Concat(Enumerable.Repeat(i, min));
+           
+        }
+        
+        if (answer.Length == 0)
+            return "-1";
+        else if(answer[0] == '0')
+            return "0";
+        
+        return answer;
+    }
+}
