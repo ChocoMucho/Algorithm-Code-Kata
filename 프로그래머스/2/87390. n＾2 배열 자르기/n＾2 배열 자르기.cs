cc8a656ce7d36316afa2aea2dccf520dc62f564d
ChocoMucho <49467508+ChocoMucho@users.noreply.github.com>
2024-08-06 19:58:04 +0900

    [level 2] Title: n^2 배열 자르기, Time: 0.85 ms, Memory: 36.2 MB -BaekjoonHub

--- 프로그래머스/2/87390. n＾2 배열 자르기/n＾2 배열 자르기.cs
+++ 프로그래머스/2/87390. n＾2 배열 자르기/n＾2 배열 자르기.cs
@@ -5,19 +5,23 @@ public class Solution {
         int[] answer = new int[right - left + 1];
         int index = 0;
         
+        // left/n는 잘라낼 부분이 시작하는 행을 가리킴        
         for(long i = left/n; i < n; ++i)
         {
             if(i*n > right)
                 break;
+            
+            // (left+index)%n는 현재 행 내의 열을 가리킴  
             for(long j = (left+index)%n; j < n; ++j)
             {
-                if((i*n + j) > right)
+                if((i*n + j) > right) //지금 가리키는 위치가 right범위를 벗어났나
                     break;
                 
                 long num = j+1;
+                // 현재 열이 현재 행 이하라면 행의 값을 사용
                 if(num <= i+1)
                     num = i+1;
-                
+            
                 answer[index++] = (int)num;
             }
         }
