a8c71b9030673434afe685462e642f10e689ec73
ChocoMucho <49467508+ChocoMucho@users.noreply.github.com>
2024-09-06 15:19:11 +0900

    [level 2] Title: 숫자 변환하기, Time: 139.90 ms, Memory: 60.5 MB -BaekjoonHub

--- 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.cs
+++ 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.cs
@@ -1,59 +1,8 @@
 using System;
-using System.Collections.Generic;
 
 public class Solution {
     public int solution(int x, int y, int n) {
         int answer = 0;
-        
-        answer = BFS(x,y,n);
-        
         return answer;
     }
-    
-    public int BFS(int x, int y, int n)
-    {
-        if(x >= y) return 0;
-        
-        HashSet<int> visited = new HashSet<int>();
-        Queue<(int val, int step)> queue = new Queue<(int, int)>();
-        queue.Enqueue((x,0));
-        visited.Add(x);
-        
-        while(queue.Count > 0)
-        {
-            (int val, int step) nowValue = queue.Dequeue();
-            if(nowValue.val == y) // 꺼낸 값이 y와 동일
-                return nowValue.step;
-            
-            //더하기n
-            int addN = nowValue.val + n;
-            if(!visited.Contains(addN) && addN <= y)
-            {
-                visited.Add(addN);
-                queue.Enqueue((addN, nowValue.step + 1));
-            }
-                
-            //곱하기2
-            int mul2 = nowValue.val*2;
-            if(!visited.Contains(mul2) && mul2 <= y)
-            {
-                visited.Add(mul2);
-                queue.Enqueue((mul2, nowValue.step + 1));
-            }
-                
-            //곱하기3
-            int mul3 = nowValue.val*3;
-            if(!visited.Contains(mul3) && mul3 <= y)
-            {
-                visited.Add(mul3);
-                queue.Enqueue((mul3, nowValue.step + 1));
-            }
-                
-        }
-        
-        return -1;
-    }
 }
-
-// BFS로 풀어야하는데..
-// 
