903ce5fcd57191940f007aac3b008fe6efedd08e
ChocoMucho <49467508+ChocoMucho@users.noreply.github.com>
2025-03-05 12:15:56 +0900

    [level 1] Title: K번째수, Time: 1.50 ms, Memory: 31.3 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.cs
+++ 프로그래머스/1/42748. K번째수/K번째수.cs
@@ -3,25 +3,23 @@ using System;
 public class Solution {
     public int[] solution(int[] array, int[,] commands) {
         int[] answer = new int[commands.GetLength(0)];
+        int[] temp;
         
         for(int i = 0; i < commands.GetLength(0); ++i)
         {
-            int a = commands[i, 0]; //시작 지점
-            int b = commands[i, 1]; //끝 지점
-            int c = commands[i, 2]; //가리킬 지점
+            int length = commands[i,1] - commands[i,0] + 1;
+            temp = new int[length];
             
-            int[] temp = new int[b - a + 1]; // 잘라낼 부분 담을 배열
-            
-            for(int n = 0; n < temp.Length; ++n)
-            {
-                temp[n] = array[(a-1)+n];
-            }
-            
+            Array.Copy(array, commands[i,0] - 1, temp, 0, length);
             Array.Sort(temp);
-            
-            answer[i] = temp[c-1];
+            answer[i] = temp[commands[i,2] - 1];
         }
         
         return answer;
     }
 }
+/*
+1. 자르고
+2. 정렬하고
+3. 고르고
+*/
