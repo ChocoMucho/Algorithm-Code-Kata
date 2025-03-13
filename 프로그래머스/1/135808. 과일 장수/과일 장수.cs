2911b32e8c0cafd57bb892d132e46e0aea016dca
ChocoMucho <49467508+ChocoMucho@users.noreply.github.com>
2025-03-13 15:10:11 +0900

    [level 1] Title: 과일 장수, Time: 163.30 ms, Memory: 50.1 MB -BaekjoonHub

--- 프로그래머스/1/135808. 과일 장수/과일 장수.cs
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.cs
@@ -1,28 +1,25 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
-
 
 public class Solution {
     public int solution(int k, int m, int[] score) {
         int answer = 0;
         
-        List<int> box = new List<int>();
-        
         Array.Sort(score);
         Array.Reverse(score);
         
-        for(int i = 0; i < score.Length; ++i)
+        for(int i = m - 1; i < score.Length; i += m)
         {
-            box.Add(score[i]);
-            if(i % m == (m - 1))
-            {
-                answer += box.Min() * m;
-                box.Clear();
-            }
+            answer += score[i] * m;
         }
         
         return answer;
     }
 }
+
+/*
+각 상자마다 제일 낮은 점수 기준이기 때문에
+한 상자에 낮은 점수들을 몰아줘야 한다. -> 내림차순 정렬(Sort, Reverse)
+[1,2,3,1,2,3,1]->(내림차순 정렬)->[3,3,2,2] [1,1,1] 
+-> 2점짜리 상자, 1점짜리는 버리기
+2점 * m * 온전한 상자 1개 => 8의 이익
+*/
