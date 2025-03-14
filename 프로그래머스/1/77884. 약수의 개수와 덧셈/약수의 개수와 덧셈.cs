89713fc10b43ae6e28ca1ed73f66c01d93572652
ChocoMucho <49467508+ChocoMucho@users.noreply.github.com>
2025-03-14 11:34:53 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 0.32 ms, Memory: 31.3 MB -BaekjoonHub

--- 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.cs
+++ 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.cs
@@ -4,20 +4,25 @@ public class Solution {
     public int solution(int left, int right) {
         int answer = 0;
         
-        for(int i=left; i<=right; ++i)
+        for(int i = left; i <= right; ++i)
         {
-            int count = 0;
-            for(int j=1; j*j<=i; ++j)
-            {
-                if(j * j  == i)
-                    ++count;
-                else if(i % j == 0)
-                    count += 2;
-            }
-            
-            answer += count % 2 == 0? i : -i;            
+            answer += DivisorCounter(i) % 2 == 0 ? i : -i;
         }
         
         return answer;
+    }
+    
+    public int DivisorCounter(int num)
+    {
+        int count = 0;
+        for(int i = 1; i * i <= num; ++i)
+        {
+            if(i*i == num)
+                ++count;
+            else if(num%i == 0)
+                count += 2;
+        }
+        
+        return count;
     }
 }
