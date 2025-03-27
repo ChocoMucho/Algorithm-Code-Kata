ef9aa32f3dd5e07187423ad1b6c7a387aef6e279
ChocoMucho <49467508+ChocoMucho@users.noreply.github.com>
2025-03-27 20:43:49 +0900

    [level 2] Title: 올바른 괄호, Time: 1.47 ms, Memory: 31.1 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.cs
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.cs
@@ -1,26 +1,26 @@
 using System;
+using System.Collections.Generic;
 
 public class Solution {
     public bool solution(string s) {
-        bool answer = false;
-        int check = 0;
+        bool answer = true;
+        Stack<char> stack = new Stack<char>();
         
-        if(s[0] == ')') return false;
-        if(s[s.Length - 1] == '(') return false;
-           
         for(int i = 0; i < s.Length; ++i)
         {
             if(s[i] == '(')
-                ++check;
+                stack.Push('(');
             else
-                --check;
-            
-            if(check < 0)
-                break;
+            {
+                if(stack.Count > 0)
+                    stack.Pop();
+                else
+                    return false;
+            }
         }
         
-        if(check == 0) answer = true;
+        if(stack.Count != 0) return false;
         
         return answer;
     }
-}
+} 
