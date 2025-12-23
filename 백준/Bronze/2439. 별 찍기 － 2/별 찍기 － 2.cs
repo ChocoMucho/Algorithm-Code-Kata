209a074a0bf4a2dd6d4eda9cd1677d2488fd4c67
ChocoMucho <49467508+ChocoMucho@users.noreply.github.com>
2025-12-23 18:50:50 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 48 ms, Memory: 5556 KB -BaekjoonHub

---
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.cs
@@ -0,0 +1,15 @@
+namespace BAEKJOON_Csharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= count; ++i)
+            {
+                Console.WriteLine(new string(' ',count-i) + new string('*', i));
+            }
+        }
+    }
+}
